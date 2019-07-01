ede032b5d249c80454b66f9bfe7305288fe81aad
Amy Quinn <amyequinn@hotmail.co.uk>
2019-07-01 16:55:09 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'authors/edit'
+
+  get 'authors/index'
+
+  get 'authors/new'
+
+  get 'authors/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :posts
+
 end
